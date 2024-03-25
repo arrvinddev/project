c20ba2bb62aee8b27926e3322161017150ebda37
arrvind <141898022+arrvindops@users.noreply.github.com>
2024-03-25 12:57:53 +0530

    testing all sg

--- learn-terraform/02-data-sources/main.tf
+++ learn-terraform/02-data-sources/main.tf
@@ -1,8 +1,16 @@
-data "aws_security_group" "selected" {
-  name="allow-all"
+# data "aws_security_group" "selected" {
+#   name="allow-all"
+# }
+
+# output "security_group_id" {
+#     value = coalesce(data.aws_security_group.selected.id, "")
+# }
+
+
+data "aws_security_group" "test" {
+  
 }
 
-output "security_group_id" {
-    value = coalesce(data.aws_security_group.selected.id, "")
+output "all_sg" {
+  value = data.aws_security_group.test
 }
-
