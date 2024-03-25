482ce6d3e2941a5fb46bd113b8b674074921c29a
arrvind <141898022+arrvindops@users.noreply.github.com>
2024-03-25 13:20:02 +0530

    null resources

--- learn-terraform/03-loops/count/main.tf
+++ learn-terraform/03-loops/count/main.tf
@@ -2,3 +2,12 @@ resource "null_resource" "null" {
 
     count = 10
 }
+
+resource "null_resource" "fruits" {
+
+    count = length(var.fruits)
+}
+
+variable "fruits" {
+    default = ["apple","banana","orange"]
+}
