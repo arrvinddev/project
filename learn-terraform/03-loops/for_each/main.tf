4fd141aac3b49074ba39990429705ba8634fd1fa
arrvind <141898022+arrvindops@users.noreply.github.com>
2024-03-26 11:52:18 +0530

    for each

--- learn-terraform/03-loops/for_each/main.tf
+++ learn-terraform/03-loops/for_each/main.tf
@@ -4,7 +4,7 @@ resource "null_resource" "fruits" {
     for_each = var.fruits
 
     provisioner "local-exec" {
-      command = "echo Fruit Name - ${var.fruits[each.value["name"]]}"
+      command = "echo Fruit Name - ${each.key}"
     # command = "echo Fruit Name - ${length(var.fruits)}"
 
 }
@@ -12,19 +12,29 @@ resource "null_resource" "fruits" {
 
 
 
+# variable "fruits" {
+#     default = {
+#       apple = {
+#       name = "apple" 
+#       count = 10
+#       }
+#       orange  = {
+#       name = "orange" 
+#       count = 200
+#       }
+#       banana  = {
+#       name = "banana" 
+#       count = 10
+#       }
+#     }
+# }
+
 variable "fruits" {
     default = {
-      apple = {
-      name = "apple" 
-      count = 10
-      }
-      orange  = {
-      name = "orange" 
-      count = 200
-      }
-      banana  = {
-      name = "banana" 
-      count = 10
-      }
+        apple = 10
+        banana = 100
+        oranges =200
+        
+        
     }
 }
