fe7148c2b26a6d42c09e2e18d9e1d7ff8d28a3a0
arrvind <141898022+arrvindops@users.noreply.github.com>
2024-03-26 13:03:12 +0530

    s3 bucket remote state

--- learn-terraform/roboshop-terraform/servers.tf
+++ learn-terraform/roboshop-terraform/servers.tf
@@ -1,69 +1,69 @@
 data "aws_ami" "centos" {
   most_recent = true
   owners      = ["973714476881"]
-  name_regex = "Centos-8-DevOps-Practice"
-  
+  name_regex  = "Centos-8-DevOps-Practice"
+
 }
 
 data "aws_security_group" "allow-all" {
-  name="allow-all"
+  name = "allow-all"
 }
 
 variable "components" {
-   default = {
+  default = {
     frontend = {
-    name = "frontend"
-    instance_type = "t3.small"
-   }
+      name          = "frontend"
+      instance_type = "t3.small"
+    }
     mongo = {
-    name = "mongo"
-    instance_type = "t3.small"
-   }
+      name          = "mongo"
+      instance_type = "t3.small"
+    }
 
     catalogue = {
-    name = "catalogue"
-    instance_type = "t3.small"
-   }
+      name          = "catalogue"
+      instance_type = "t3.small"
+    }
 
-   redis = {
-    name = "redis"
-    instance_type = "t3.small"
-   }
+    redis = {
+      name          = "redis"
+      instance_type = "t3.small"
+    }
 
-user = {
-    name = "user"
-    instance_type = "t3.small"
-   }
+    user = {
+      name          = "user"
+      instance_type = "t3.small"
+    }
 
-   cart = {
-    name = "cart"
-    instance_type = "t3.small"
-   
-   }
+    cart = {
+      name          = "cart"
+      instance_type = "t3.small"
 
-   rabbitmq = {
-    name = "rabbitmq"
-    instance_type = "t3.small"
-   }
-   mysql = {
-    name = "mysql"
-    instance_type = "t3.small"
-   }
-   
-   shipping = {
-    name = "shipping"
-    instance_type = "t3.small"
-   }
+    }
 
-   payment = {
-    name = "payment"
-    instance_type = "t3.small"
-   }
-   
-   
-   }
+    rabbitmq = {
+      name          = "rabbitmq"
+      instance_type = "t3.small"
+    }
+    mysql = {
+      name          = "mysql"
+      instance_type = "t3.small"
+    }
 
-   }
+    shipping = {
+      name          = "shipping"
+      instance_type = "t3.small"
+    }
+
+    payment = {
+      name          = "payment"
+      instance_type = "t3.small"
+    }
+
+
+  }
+
+}
 
 
 
@@ -72,9 +72,9 @@ user = {
 
 
 resource "aws_instance" "instance" {
-  ami           = data.aws_ami.centos.image_id
-  for_each   =   var.components
-  instance_type = each.value["instance_type"]
+  ami                    = data.aws_ami.centos.image_id
+  for_each               = var.components
+  instance_type          = each.value["instance_type"]
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
   tags = {
     Name = each.value["name"]
@@ -86,11 +86,11 @@ resource "aws_instance" "instance" {
 
 resource "aws_route53_record" "records" {
   for_each = var.components
-  zone_id = "Z05216872DPXAQS543510"
-  name    = "${each.value["name"]}-dev.arrvind.tech"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.instance[each.value["name"]].private_ip]
+  zone_id  = "Z05216872DPXAQS543510"
+  name     = "${each.value["name"]}-dev.arrvind.tech"
+  type     = "A"
+  ttl      = 30
+  records  = [aws_instance.instance[each.value["name"]].private_ip]
 }
 
 
