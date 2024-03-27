ddd8142b158d4dd06ce21457e3ccbae1535b4bb6
arrvind <141898022+arrvindops@users.noreply.github.com>
2024-03-27 14:30:59 +0530

    dev and prod folders with separate backend

--- servers.tf
+++ servers.tf
@@ -1,69 +1,6 @@
-data "aws_ami" "centos" {
-  most_recent = true
-  owners      = ["973714476881"]
-  name_regex  = "Centos-8-DevOps-Practice"
-
-}
-
-data "aws_security_group" "allow-all" {
-  name = "allow-all"
-}
-
-variable "components" {
-  default = {
-    frontend = {
-      name          = "frontend"
-      instance_type = "t3.small"
-    }
-    mongo = {
-      name          = "mongo"
-      instance_type = "t3.small"
-    }
-
-    catalogue = {
-      name          = "catalogue"
-      instance_type = "t3.small"
-    }
-
-    redis = {
-      name          = "redis"
-      instance_type = "t3.small"
-    }
-
-    user = {
-      name          = "user"
-      instance_type = "t3.small"
-    }
-
-    cart = {
-      name          = "cart"
-      instance_type = "t3.small"
-
-    }
-
-    rabbitmq = {
-      name          = "rabbitmq"
-      instance_type = "t3.small"
-    }
-    mysql = {
-      name          = "mysql"
-      instance_type = "t3.small"
-    }
-
-    shipping = {
-      name          = "shipping"
-      instance_type = "t3.small"
-    }
-
-    payment = {
-      name          = "payment"
-      instance_type = "t3.small"
-    }
 
 
-  }
 
-}
 
 
 
