27910bc2d515d0382d55992d68c85f7433633eaf
arrvind <141898022+arrvindops@users.noreply.github.com>
2024-03-27 15:03:34 +0530

    lookup

--- learn-terraform/04-functions/main.tf
+++ learn-terraform/04-functions/main.tf
@@ -5,3 +5,22 @@ variable "sample"{
 output "sample"{
     value = upper(var.sample)
 }
+
+variable "sample1"{
+    default= ["abc","xyz"]
+}
+
+output "sample1"{
+    value = element(var.sample1, 0)
+}
+
+variable "sample2"{
+    default= {
+        xyz = 100
+        abc = 200
+    }
+}
+
+output "sample1"{
+    value = lookup(var.sample2, "a1", "dummy")
+}
