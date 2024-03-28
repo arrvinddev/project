380e37a232633fa7b83f2a6ceb78ed5b6d945b9a
arrvind <141898022+arrvindops@users.noreply.github.com>
2024-03-28 14:02:31 +0530

    got the instance type and ami id

--- learn-terraform/07-import/main.tf
+++ learn-terraform/07-import/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "workstation" {
-    ami = "unknown"
-    instance_type = "unknown"
+    ami = "ami-0f3c7d07486cad139"
+    instance_type = "t2.small"
 }
 
 output "private_ip" {
