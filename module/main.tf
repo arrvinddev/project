7cd4bf62ee983c4815f311b63b792cff4b56c5e2
arrvind <141898022+arrvindops@users.noreply.github.com>
2024-03-28 13:48:28 +0530

    import existing ec2

--- module/main.tf
+++ module/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "instance" {
   instance_type          = var.instance_type
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
   tags = {
-    Name = var.component_name
+    Name = local.name
   }
 }
 
