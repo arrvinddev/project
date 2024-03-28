036b4037cb9490be6f89d94cf6ed961c597b8aed
arrvind <141898022+arrvindops@users.noreply.github.com>
2024-03-28 13:33:34 +0530

    iterating a module is better than iterating a resource

---
+++ module/main.tf
@@ -0,0 +1,51 @@
+resource "aws_instance" "instance" {
+  ami                    = data.aws_ami.centos.image_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+  tags = {
+    Name = var.component_name
+  }
+}
+
+resource "null_resource" "provisioner" {
+  depends_on = [ aws_instance.instance,aws_route53_record.records ]
+  provisioner "remote-exec" {
+
+  connection {
+    type     = "ssh"
+    user     = "root"
+    password = "DevOps321"
+    host     = aws_instance.instance.private_ip
+  }
+
+    inline = [
+      "rm -rf roboshop-shell",
+      "git clone https://github.com/raghudevopsb72/roboshop-shell",
+      "cd roboshop-shell",
+      "sudo bash ${var.component_name}.sh ${var.password}}"
+    ]
+
+
+    
+  
+  }
+
+
+}
+  
+
+
+
+
+resource "aws_route53_record" "records" {
+
+  zone_id  = "Z05216872DPXAQS543510"
+  name     = "${var.component_name}-dev.arrvind.tech"
+  type     = "A"
+  ttl      = 30
+  records  = [aws_instance.instance.private_ip]
+}
+
+
+
+
