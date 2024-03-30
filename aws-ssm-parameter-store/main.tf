53a587521370c2a48c1373c839fc08f082b80447
arrvind <141898022+arrvindops@users.noreply.github.com>
2024-03-30 16:08:04 +0530

    aws ssm parameter store

--- aws-ssm-parameter-store/main.tf
+++ aws-ssm-parameter-store/main.tf
@@ -1,6 +1,7 @@
 resource "aws_ssm_parameter" "parameter" {
-  name  = "test.test1"
-  value = "Hello World"
+  count = length(var.parameters)
+  name  = var.parameters[count.index].name
+  value = var.parameters[count.index].value
   type  = "SecureString"
   key_id = "ca96756f-0bb2-4402-b6ae-7383aa4f1c79"
 }
