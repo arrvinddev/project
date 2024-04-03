117914422d03bd5a45d87b3da24988d1b21ca10f
arrvind <141898022+arrvindops@users.noreply.github.com>
2024-04-03 16:03:36 +0530

    ssm passwords

--- aws-ssm-parameter-store/main.tf
+++ aws-ssm-parameter-store/main.tf
@@ -6,7 +6,7 @@ resource "aws_ssm_parameter" "parameter" {
   key_id = "ca96756f-0bb2-4402-b6ae-7383aa4f1c79"
 }
 
-resource "aws_ssm_parameter" "parameter" {
+resource "aws_ssm_parameter" "passwords" {
   count = length(var.passwords)
   name  = var.passwords[count.index].name
   value = var.passwords [count.index].value
