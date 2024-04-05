218beeb525832f79b02a649f72ebf6984bfe5ebf
arrvind <141898022+arrvindops@users.noreply.github.com>
2024-04-05 12:17:23 +0530

    fetching via ansible pull , checking all roboshop instances

--- module/locals.tf
+++ module/locals.tf
@@ -8,8 +8,7 @@ locals {
       "sudo bash ${var.component_name}.sh ${var.password}}"
     ]
   app_commands = [
-    # "sudo labauto ansible",
-    # "ansible-pull -i localhost, -U https://github.com/arrvinddev/ansible-project.git roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
-    "echo Ok"
+    "sudo labauto ansible",
+    "ansible-pull -i localhost, -U https://github.com/arrvinddev/ansible-project.git roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
   ]
 }
