078ac1d71be141893924f7151a5ae3ba124f1ed6
arrvind <141898022+arrvindops@users.noreply.github.com>
2024-04-05 10:35:09 +0530

    iam role for ansible pull

--- module/main.tf
+++ module/main.tf
@@ -42,5 +42,29 @@ resource "aws_route53_record" "records" {
 }
 
 
+resource "aws_iam_role" "test_role" {
+  name = "${var.component_name}-${var.env}-role"
+
+  
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "${var.component_name}-${var.env}-role"
+  }
+}
+
+
 
 
