688b770a894cf1ff77e33e33238f887890cc5bb7
arrvind <141898022+arrvindops@users.noreply.github.com>
2024-04-11 12:50:01 +0530

    attaching tags for db and app servers

--- module/locals.tf
+++ module/locals.tf
@@ -11,4 +11,10 @@ locals {
     "sudo labauto ansible",
     "ansible-pull -i localhost, -U https://github.com/arrvinddev/ansible-project.git roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
   ]
+  db_tags = {
+    Name = "${var.component_name}-${var.env}"
+  }
+  app_tags = {
+    Name = "${var.component_name}-${var.env}"
+  }
 }
