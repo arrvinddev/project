29407bc7a6806341c04947ea217f040361fd4255
arrvind <141898022+arrvindops@users.noreply.github.com>
2024-04-15 08:40:45 +0530

    module source

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vpc"{
-    source = "git@https://github.com/arrvinddev/tf-module-vpc.git"
+    source = "git::https://github.com/arrvinddev/tf-module-vpc.git"
 
     for_each = var.vpc
     cidr_block = each.value["cidr_block"]
