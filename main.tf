01e3d2f3dcbf35e834c416c9eb4cfdfa7f8429c8
arrvind <141898022+arrvindops@users.noreply.github.com>
2024-04-18 00:15:23 +0530

    declaration of kms_arn and docdb

--- main.tf
+++ main.tf
@@ -10,38 +10,39 @@ module "vpc"{
     default_vpc_cidr = var.default_vpc_cidr
     default_vpc_rtid = var.default_vpc_rtid
 }
-# module "app"{
-# source = "git::https://github.com/arrvinddev/tf-module-app.git"
-# for_each = var.app
-# instance_type = each.value["instance_type"]
-# name = each.value["name"]
-# desired_capacity = each.value["desired_capacity"]
-# max_size = each.value["max_size"]
-# min_size = each.value["min_size"]
-# bastion_cidr = var.bastion_cidr
-# tags = local.tags
-# subnet_ids = lookup(lookup(lookup(lookup(module.vpc,"main",null),"subnets",null),each.value["subnet_name"],null),"subnet_ids",null)
-# env = var.env
-# vpc_id = lookup(lookup(module.vpc,"main",null),"vpc_id",null)
-# allow_app_cidr= lookup(lookup(lookup(lookup(module.vpc,"main",null),"subnets",null),each.value["allow_app_cidr"],null),"subnet_cidrs",null)
-# }
+module "app"{
+source = "git::https://github.com/arrvinddev/tf-module-app.git"
+for_each = var.app
+instance_type = each.value["instance_type"]
+name = each.value["name"]
+desired_capacity = each.value["desired_capacity"]
+max_size = each.value["max_size"]
+min_size = each.value["min_size"]
+bastion_cidr = var.bastion_cidr
+tags = local.tags
+subnet_ids = lookup(lookup(lookup(lookup(module.vpc,"main",null),"subnets",null),each.value["subnet_name"],null),"subnet_ids",null)
+env = var.env
+vpc_id = lookup(lookup(module.vpc,"main",null),"vpc_id",null)
+allow_app_cidr= lookup(lookup(lookup(lookup(module.vpc,"main",null),"subnets",null),each.value["allow_app_cidr"],null),"subnet_cidrs",null)
+}
 
 
-# module "docdb"{
-# source = "git::https://github.com/arrvinddev/tf-module-docdb.git"
-# for_each = var.docdb
-# engine_version = each.value["engine_version"]
-# instance_count = each.value["instance_count"]
-# instance_class = each.value["instance_class"]   
-# tags = local.tags
-# subnets = lookup(lookup(lookup(lookup(module.vpc,"main",null),"subnets",null),each.value["subnet_name"],null),"subnet_ids",null)
-# env = var.env
-# vpc_id = local.vpc_id
-# allow_db_cidr= lookup(lookup(lookup(lookup(module.vpc,"main",null),"subnets",null),each.value["allow_db_cidr"],null),"subnet_cidrs",null)
+module "docdb"{
+source = "git::https://github.com/arrvinddev/tf-module-docdb.git"
+for_each = var.docdb
+engine_version = each.value["engine_version"]
+instance_count = each.value["instance_count"]
+instance_class = each.value["instance_class"]   
+tags = local.tags
+subnets = lookup(lookup(lookup(lookup(module.vpc,"main",null),"subnets",null),each.value["subnet_name"],null),"subnet_ids",null)
+env = var.env
+vpc_id = local.vpc_id
+kms_arn = var.kms_arn
+allow_db_cidr= lookup(lookup(lookup(lookup(module.vpc,"main",null),"subnets",null),each.value["allow_db_cidr"],null),"subnet_cidrs",null)
 
 
 
-# }
+}
 
 
 
