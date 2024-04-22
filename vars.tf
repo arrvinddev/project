111591a9e0ab4fbcfb51c7b626d0df10a4c325ba
arrvind <141898022+arrvindops@users.noreply.github.com>
2024-04-22 12:06:57 +0530

    empty space

--- vars.tf
+++ vars.tf
@@ -6,7 +6,7 @@ variable "default_vpc_cidr" {}
 variable "default_vpc_id" {}
 variable "default_vpc_rtid" {}
 variable "domain_id" {}
-variable "domain_name " {}
+variable "domain_name" {}
 variable "docdb" {
   
 }
