dd0f2f069a953fc2ca2915e1eda7cd607749d411
arrvind <141898022+arrvindops@users.noreply.github.com>
2024-05-01 10:29:51 +0530

    stage release application

--- roboshop-jenkins/vars/maven.groovy
+++ roboshop-jenkins/vars/maven.groovy
@@ -25,8 +25,9 @@ stages {
     
     stage ('Code Quality'){
         steps {
-            sh 'ls -l'
+            // sh 'ls -l'
             // sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.29.145:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true -Dsonar.java.binaries=./target'
+            sh "echo Code Quality"
         }
     }
 
